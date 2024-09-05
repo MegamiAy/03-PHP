79eec82855c5139787b2cbeb54d62afba0d57a01
Laiz Detros <megami.ay@gmail.com>
2024-09-05 11:46:32 -0300

    Update index_queCadastraOsDados.php

--- exercicios /exemplos /ex_painel_controle /index_queCadastraOsDados.php
+++ exercicios /exemplos /ex_painel_controle /index_queCadastraOsDados.php
@@ -1,5 +1,29 @@
-<?php
+<html>
+    <head></head>
+    <body>
+        <!--    Editar Sobre   -->
+        <div class="col-md-9">
+	      <?php 
 
+              if(isset($_POST['editar_sobre'])){
+                $sobre = $_POS['sobre'];
+                echo '<div class="alert alert-success" role="alert">O código HTML <b>Sobre</b> foi editado com sucesso!</div>'
+              }
+        
+        ?>
+      		<div id="sobre_section">
+      			<h3>Sobre</h3>
+      			<form method="POST">
+      			      <!--        label          -->
+              		<!--        Textarea   name="sobre"       -->
+              		<!--        input hidden name="editar_sobre"   value=""    -->        			<!--        button submit name="acao"    -->
+              		</form>
+      		</div>
+      	</div>
+        <!--    Cadastrar Equipe   -->
+        <form method="POST">
+        </form>
+    </body>
+</html>
 
-
-?>
+<script></script>
