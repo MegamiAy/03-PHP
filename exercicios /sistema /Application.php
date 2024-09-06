218fab2cfcc08555c74f8773a1080fbcc62710e4
Laiz Detros <megami.ay@gmail.com>
2024-09-06 11:02:42 -0300

    Update Application.php

--- exercicios /sistema /Application.php
+++ exercicios /sistema /Application.php
@@ -1,10 +1,17 @@
 <?php
     class Application {
 
-        function __construct(){
-            
+        public function executar(){
+            $url = isset($_GET['url']) ? explode('/',$_GET['url'])[0] : 'Home';         //
+            $url = ucfirst($url);
+            $url.="Controller";
+            if(file_exists('controllers/'.$url.'.php')){
+                $controler = new $url();
+                $controler->executar();
+            } else {
+                die("Controlador Inexistente");
+            }
         }
 
     }
-
 ?>
