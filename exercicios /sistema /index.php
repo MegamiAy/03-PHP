ce393297ac44738a10efc92a0602340e479099d8
Laiz Detros <megami.ay@gmail.com>
2024-09-06 11:57:16 -0300

    Update index.php

--- exercicios /sistema /index.php
+++ exercicios /sistema /index.php
@@ -1,1 +1,11 @@
+<?php
+    $autoload = function($class){               /// criar função que vai carregar as classes automaticamente
+        include($class.'.php');
+    };
 
+    spl_autoload_register($autoload);
+
+    $app = new Application();           //instanciando a classe
+    $app->executar();                   //executando a classe
+
+?>
