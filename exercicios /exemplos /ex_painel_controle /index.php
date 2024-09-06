359f84012751319baa3a5d68463f1119000ece9a
Laiz Detros <megami.ay@gmail.com>
2024-09-06 09:11:48 -0300

    Update index.php

--- exercicios /exemplos /ex_painel_controle /index.php
+++ exercicios /exemplos /ex_painel_controle /index.php
@@ -5,23 +5,48 @@
     $sobre = $sobre->fetch()['Sobre'];
 ?>
 
-<html>
+<html>	
     <head></head>
-    <body>
-	<!--    Home   -->
-	<div id="sobre"><?php echo $sobre; ?></div>
+    
+<!--  outro index.php	 -->
+<!--    Home   --> 
+ 	<div id="sobre"><?php echo $sobre; ?></div
+ 	<div id="equipe">
+		<?php 
+			$selectMembros = $pdo->prepare("SELECT * FROM `tb_equipe`");
+			$selectMembros->execute();
+			$membros = $selectMembros->fetchAll();
+			for($i = 0; $i < count($membros); $i++){
+		?>
+		<!-- 		divs e etc		 -->
+		<!-- 	h1. h2 com echo em $membros[$i]['nome']	 -->
+		<!-- 	p com echo em $membros[$i]['desc']	 -->
+		<?php } ?>
+	</div>
+
+<!-- index.php do painel -->
+<body>
 	    
         <!--    Editar Sobre   -->
         <div class="col-md-9">
 	      <?php 
 
               if(isset($_POST['editar_sobre'])){
-                $sobre = $_POS['sobre'];
-		$pdo->exec("DELETE FROM `tb_sobre`");
-		$sql = $pdo->prepare("INSERT INTO `tb_sobre` VALUES(null, ?)");
-		$sql->execute(array($sobre));
-                echo '<div class="alert alert-success" role="alert">O código HTML <b>Sobre</b> foi editado com sucesso!</div>'
-              }
+        		$sobre = $_POS['sobre'];
+			$pdo->exec("DELETE FROM `tb_sobre`");
+			$sql = $pdo->prepare("INSERT INTO `tb_sobre` VALUES(null, ?)");
+			$sql->execute(array($sobre));
+               		echo '<div class="alert alert-success" role="alert">O código HTML <b>Sobre</b> foi editado com sucesso!</div>';
+			$sobre = $pdo->prepare("SELECT * FROM `tb_sobre`");
+			$sobre->execute();
+			$sobre = $sobre->fetch()['Sobre'];
+              } else if (isset($_POST['cadastrar_equipe'])){
+		     	$nome = $_POST['nome'];
+		      	$descricao = $_POST['desc'];
+		      	$sql = $pdo->prepare("INSERT INTO `tb_equipe` VALUES(null, ?, ?)");
+			$sql->execute(array($nome, $descricao));
+	                echo '<div class="alert alert-success" role="alert">O integrante da equipe foi cadastrado com sucesso!</div>';
+	      }
         
         ?>
       		<div id="sobre_section">
@@ -33,10 +58,47 @@
 			<!--        button submit name="acao"    -->
               		</form>
       		</div>
+		<div id="equipe_section">
+      			<h3>Cadastrar Edit</h3>
+      			<form method="POST">
+      			<!--        label          -->
+			<!-- 	    Input	name: nome	-->
+      			<!--        label          -->
+			<!-- 	    Input	name: desc	-->
+              		<!--        input hidden name="cadastrar_equipe"   value=""    -->
+			<!--        button submit name="acao"    -->
+              		</form>
+      		</div>
+		<div id="membros-lista">
+			<table>
+				<thead>
+					<tr>
+						<th>ID</th>
+						<th>Nome</th>
+						<th>#</th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php 
+						$selectMembros = $pdo->prepare("SELECT * FROM `tb_equipe`");
+						$selectMembros->execute();
+						$membros = $selectMembros->fetchAll();
+						foreach($membros as $key=>value){
+					?>
+					<tr>
+						<td><?php echo $value['id']?></td>
+						<td><?php echo $value['nome']?></td>
+						<td><!-- button com icon. para deletar --></td>
+					</tr>
+					<?php } ?>
+				</tbody>
+			</table>
+
+		</div>
       	</div>
-        <!--    Cadastrar Equipe   -->
-        <form method="POST">
-        </form>
+	<div>
+		
+	</div>
     </body>
 </html>
 
