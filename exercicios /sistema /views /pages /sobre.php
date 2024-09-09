024502ef90e7c7d56da54c1e0e5cb46799cdaaca
Laiz Detros <megami.ay@gmail.com>
2024-09-09 11:13:30 -0300

    Update sobre.php

--- exercicios /sistema /views /pages /sobre.php
+++ exercicios /sistema /views /pages /sobre.php
@@ -1,1 +1,9 @@
-<h1 style="background: grey;"><?php echo $arr['titulo']; ?></h1>
+<div class="chamada">
+    <div class="center">
+        <h2><?php echo $arr['titulo']; ?></h2>
+    </div><!-- center -->
+</div><!-- chamada -->
+<div class="sobre">
+    <h2>Sobre a empresa</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident rerum recusandae, magni voluptatem repudiandae facere, nostrum, vero quae iste iusto quod optio. Porro ea quos maiores ducimus excepturi, ex pariatur?</p>
+</div><!-- sobre -->
