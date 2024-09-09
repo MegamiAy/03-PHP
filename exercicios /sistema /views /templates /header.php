7c9e67cf19142dcac216355dbb919092d66f5d5f
Laiz Detros <megami.ay@gmail.com>
2024-09-09 10:49:29 -0300

    Update header.php

--- exercicios /sistema /views /templates /header.php
+++ exercicios /sistema /views /templates /header.php
@@ -2,7 +2,8 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <title>Título</title>
+    <title><?php echo $arr['titulo']; ?></title>
+    <link href="<?php echo INCLUDE_PATH_FULL ?>css/style.css" rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
     <h1>Header</h1>
