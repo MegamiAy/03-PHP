e5604801ef12b56c0252e9fbe2f8214cc7ca58ab
Laiz Detros <megami.ay@gmail.com>
2024-09-09 11:12:58 -0300

    Update header.php

--- exercicios /sistema /views /templates /header.php
+++ exercicios /sistema /views /templates /header.php
@@ -6,6 +6,19 @@
     <link href="<?php echo INCLUDE_PATH_FULL ?>css/style.css" rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-    <h1>Header</h1>
+<header>
+    <div class="center">
+        <div class="logo">
+            <h1> :) </h1>
+        </div> <!-- logo -->
+        <nav class="menu">
+            <?php
+                foreach($this->menuItems as $key => $value){
+                    echo '<a href="'.INCLUDE_PATH.strtolower($value).'"> '.$value.' </a>';
+                }
+            ?>
+        </nav>
+    </div> <!-- center -->
+</header>
 
 
