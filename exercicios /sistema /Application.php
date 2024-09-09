75513b3e930d52db2cda945b6e8b836b4471e02d
Laiz Detros <megami.ay@gmail.com>
2024-09-09 10:49:41 -0300

    Update Application.php

--- exercicios /sistema /Application.php
+++ exercicios /sistema /Application.php
@@ -1,10 +1,13 @@
 <?php
+    define('INCLUDE_PATH_FULL','http://127.0.0.1:8080/simples/teste_mvc/views/pages/');
+
     class Application{
 
         public function executar(){
             $url = isset($_GET['url']) ? explode('/', $_GET['url']) : array('Home');
             $url = ucfirst($url[0]);
             $url .= "Controller";
+            
 
             if(file_exists('controllers/'.$url.'.php')){
                 $className = 'Controllers\\'.$url;
