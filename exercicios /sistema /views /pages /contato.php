d1566213fd13e2c21036da3401eb3ef885a4bf4c
Laiz Detros <megami.ay@gmail.com>
2024-09-09 11:13:52 -0300

    Update contato.php

--- exercicios /sistema /views /pages /contato.php
+++ exercicios /sistema /views /pages /contato.php
@@ -1,1 +1,12 @@
-<h1 style="background: grey;"><?php echo $arr['titulo']; ?></h1>
+<div class="chamada">
+    <div class="center">
+        <h2><?php echo $arr['titulo']; ?></h2>
+    </div><!-- center -->
+</div><!-- chamada -->
+<div class="contato">
+    <form method="$_POST">
+        <input type="text" name="nome" id="nome" placeholder="nome...">
+        <textarea name="mensagem" id="mensagem" placeholder="Digite sua mensagem..."></textarea>
+        <input type="submit" name="acao" value="Enviar">
+    </form>
+</div><!-- contato -->
