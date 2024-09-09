22f793fc9ffeafb87176363eb2e1df782b120516
Laiz Detros <megami.ay@gmail.com>
2024-09-09 10:21:41 -0300

    Update and rename ContatoView.php to MainView.php

---
+++ exercicios /sistema /views /MainView.php
@@ -0,0 +1,23 @@
+<?php
+
+    namespace Views;
+    class MainView{
+
+        private $viewName;
+        private $header;
+        private $footer;
+
+        public function __construct($viewName, $header = 'header', $footer = 'footer'){
+            $this->viewName = $viewName;
+            $this->header = $header;
+            $this->footer = $footer;
+        }
+
+        public function render(){
+            include('pages/templates/'.$this->header.'.php');
+            include('pages/'.$this->viewName.'.php');
+            include('pages/templates/'.$this->footer.'.php');
+        }
+    }
+
+?>
