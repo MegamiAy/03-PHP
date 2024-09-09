fb2c0f15a1e56bb5d0e8cbbc3236db0f6ebe7862
Laiz Detros <megami.ay@gmail.com>
2024-09-09 10:39:29 -0300

    Update ContatoController.php

--- exercicios /sistema /controllers /ContatoController.php
+++ exercicios /sistema /controllers /ContatoController.php
@@ -8,5 +8,9 @@
             $this->view = new MainView('contato');
         }
 
+        public function executar(){             // vai sobrescrever a func executar do Controller.php
+            $this->view->render(array('titulo'=>'Contato'));
+        }
+
     }
 ?>
