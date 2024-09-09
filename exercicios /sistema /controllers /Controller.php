d04021c2a5ea1dd52db2c05188901565adc855e8
Laiz Detros <megami.ay@gmail.com>
2024-09-09 09:58:07 -0300

    Create Controller.php

---
+++ exercicios /sistema /controllers /Controller.php
@@ -0,0 +1,14 @@
+<?php 
+    namespace Controllers;
+
+    class Controller{
+
+        protected $view;
+        protected $model;
+
+        public function executar(){
+            $this->view->render();
+        }
+
+    }
+?>
