734711d6ba26da9c953d38b87f8c134ab5a6391a
Laiz Detros <megami.ay@gmail.com>
2024-09-10 10:55:06 -0300

    Update ContatoController.php

--- exercicios /sistema /controllers /ContatoController.php
+++ exercicios /sistema /controllers /ContatoController.php
@@ -1,15 +1,33 @@
 <?php
     namespace Controllers;
-    use Views\MainView;
+
+use Models\ContatoModel;
+use Router;
+use Views\MainView;
 
     class ContatoController extends Controller{
 
-        public function __construct(){
-            $this->view = new MainView('contato');
-        }
+        // public function __construct(){
+        //     $this->view = new MainView('contato');
+        // }
 
-        public function executar(){             // vai sobrescrever a func executar do Controller.php
-            $this->view->render(array('titulo'=>'Contato'));
+        public function executar(){             // vai sobrescrever a func executar do Controller.php e adicionar novas validações etc a ela
+            if(isset($_POST['acao'])){
+                ContatoModel::enviarFormulario();
+                echo '<script>location.href="'.INCLUDE_PATH.'contato/sucesso"</script>';
+                die();
+            }
+            
+            Router::rota('contato/sucesso', function() {
+                $view = new MainView('contato_sucesso');
+                $view->render(array('titulo'=>'Contato Sucesso'));
+            });
+    
+            Router::rota('contato', function() {
+                $view = new MainView('contato');
+                $view->render(array('titulo'=>'Contato'));
+            });
+            
         }
 
     }
