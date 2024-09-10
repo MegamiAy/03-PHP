d3b363b98ad0cde74797ed69f4286589e954cd4b
Laiz Detros <megami.ay@gmail.com>
2024-09-10 10:59:45 -0300

    Update home.php

--- exercicios /sistema /views /pages /home.php
+++ exercicios /sistema /views /pages /home.php
@@ -1,9 +1,16 @@
-<div class="chamada">
-    <div class="center">
-        <h2><?php echo $arr['titulo']; ?></h2>
-    </div><!-- center -->
-</div><!-- chamada -->
-
-<div class="sobre">
-    <h2>Bem Vindo</h2>
-</div><!-- sobre -->
+<div class="container2">
+    <div class="chamada">
+        <div class="center">
+            <h2><?php echo $arr['titulo']; ?></h2>
+        </div><!-- center -->
+    </div><!-- chamada -->
+    <div class="sobre">
+        <h2>Bem Vindo</h2>
+        <article>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt eaque fugit soluta velit ipsam nobis non a consequuntur. Ea voluptatibus quo asperiores consectetur, aut necessitatibus quasi accusantium sit quidem eaque? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi possimus cum ducimus in laborum sit obcaecati ipsum, amet nihil quae voluptate corrupti blanditiis numquam. Voluptates repudiandae perferendis fugiat fugit! Nobis.</p>
+        </article>
+        <article>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt eaque fugit soluta velit ipsam nobis non a consequuntur. Ea voluptatibus quo asperiores consectetur, aut necessitatibus quasi accusantium sit quidem eaque? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi possimus cum ducimus in laborum sit obcaecati ipsum, amet nihil quae voluptate corrupti blanditiis numquam. Voluptates repudiandae perferendis fugiat fugit! Nobis.</p>
+        </article>
+    </div><!-- sobre -->
+</div>
