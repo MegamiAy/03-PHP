0e2d1ac0240e1f724e6f0f6b172e21d964048411
Laiz Detros <megami.ay@gmail.com>
2024-09-10 10:59:24 -0300

    Create contato_sucesso.php

---
+++ exercicios /sistema /views /pages /contato_sucesso.php
@@ -0,0 +1,10 @@
+<div class="container">
+    <div class="chamada">
+        <div class="center">
+            <h2><?php echo $arr['titulo']; ?></h2>
+        </div><!-- center -->
+    </div><!-- chamada -->
+    <div class="contato">
+        <p>Mensagem enviada com sucesso</p>
+    </div><!-- contato -->
+</div>
